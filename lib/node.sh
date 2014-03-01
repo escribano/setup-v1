99d50d2675092cff5e7f17c409a6fab4c5c3d57a
Ademir Escribano <ademir.escribano@gmail.com>
2014-03-01 01:03:01 -0300

    node

--- lib/node.sh
+++ lib/node.sh
@@ -2,7 +2,8 @@ function install.node.latest {
   # apt-get update && apt-get upgrade -y
   # apt-get install build-essential python openssl libssl-dev curl
   # apt-get install python g++ make checkinstall
-
+  SAVED_PWD=`pwd`
+  
   cd ~
   mkdir src ; cd src
   wget -N http://nodejs.org/dist/node-latest.tar.gz
@@ -11,6 +12,8 @@ function install.node.latest {
   checkinstall -y --install=no --pkgversion 0.10.25  make -j$(($(nproc)+1)) install  # Replace with current version number.
   #dpkg -i node_*
   dpkg -i node_0.10.25-1_amd64.deb
+  
+  cd $SAVED_PWD
 
   ###
 
@@ -20,7 +23,8 @@ function install.node.25 {
   # apt-get update && apt-get upgrade -y
   # apt-get install build-essential python openssl libssl-dev curl
   # apt-get install python g++ make checkinstall
-
+  SAVED_PWD=`pwd`
+  
   cd ~
   mkdir src ; cd src
   wget -N http://nodejs.org/dist/v0.10.25/node-v0.10.25.tar.gz
@@ -30,6 +34,6 @@ function install.node.25 {
   #dpkg -i node_*
   dpkg -i node_0.10.25-1_amd64.deb
 
-  ###
+  cd $SAVED_PWD
 
 }
