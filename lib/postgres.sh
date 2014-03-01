992390dcdef1b2160c57e483e6246153023766fb
Ademir Escribano <ademir.escribano@gmail.com>
2014-03-01 02:14:48 -0300

    pg

--- lib/postgres.sh
+++ lib/postgres.sh
@@ -22,24 +22,41 @@ function move.cluster {
   mount /var/log/postgresql
 }
 
-function confit.cluster {
-  nano /etc/postgresql/9.1/main/pg_hba.conf
+function config.cluster {
+  cp /etc/postgresql/9.3/main/pg_hba.conf /etc/postgresql/9.3/main/pg_hba.conf.backup
+  echo "
+# TYPE  DATABASE        USER            ADDRESS                 METHOD
+local   all             all                                     trust
+local   all             all                                     ident map=admin
+local   all             all                                     ident
+local   all             all                                     peer
+local   all             all                                     md5
+  " > /etc/postgresql/9.3/main/include_auth_hba.conf
+  
+  sed -n 'H;${x;s/# DO NOT DISABLE!.*\n/include "include_auth_hba.conf"\n\n\n&/;p;}' /etc/postgresql/9.3/main/pg_hba.conf
+  
+  #include 'filename'
+  #include_dir 'directory'
+  #nano /etc/postgresql/9.3/main/pg_hba.conf
+  #grep "^[a-z]" /etc/postgresql/9.3/main/pg_hba.conf
+  #sed '/^JAVA_HOME/a this is the new line' filename > newfilename
+  #sed -n 'H;${x;s/nameserver .*\n/nameserver 127.0.0.1\n&/;p;}' resolv.conf
+  #sed -n 'H;${x;s/^\n//;s/nameserver .*\n/nameserver 127.0.0.1\n&/;p;}' resolv.conf
+  #sed -n 'H;${x;s/^\n//;s/nameserver .*$/nameserver 127.0.0.1\n&/;p;}' resolv.conf
   # TYPE  DATABASE        USER            ADDRESS                 METHOD
-  echo "
-  local   all             all                                     trust
-  local   all             all                                     ident map=admin
-  local   all             all                                     ident
-  local   all             all                                     peer
-  local   all             all                                     md5
-  " > include
-	
-  nano /etc/postgresql/9.1/main/pg_ident.conf
+
+	# DO NOT DISABLE!
+  #sed -n 'H;${x;s/# DO NOT DISABLE!.*\n/include "include_auth_hba.conf"\n\n\n&/;p;}' /etc/postgresql/9.3/main/pg_hba.conf
+  #sed '/^# DO NOT DISABLE!/include thefile' /etc/postgresql/9.3/main/pg_hba.conf
+  #sed -n 'H;${x;s/DISABLE .*\n/include thefile\n&/;p;}' /etc/postgresql/9.3/main/pg_hba.conf
+  #sed -n 'H;${x;s/^\n//;s/DISABLE .*$/include thefile\n&/;p;}' /etc/postgresql/9.3/main/pg_hba.conf
+  #nano /etc/postgresql/9.3/main/pg_ident.conf
   # MAPNAME       SYSTEM-USERNAME         PG-USERNAME
-  echo "
-  admin           root                    postgres
-  admin           ademir                  postgres
-  admin           www-data                postgres
-  " > include
+  #echo "
+  #admin           root                    postgres
+  #admin           ademir                  postgres
+  #admin           www-data                postgres
+  #" > include
 }
 
 function start.postgres {
@@ -57,3 +74,8 @@ function more.postgres {
   /etc/init.d/postgresql start
 }
 
+function find.postgres () {
+  ps -ef | grep postgres
+  psql -d postgres -c "SHOW hba_file;"
+  cat /usr/local/var/postgres/pg_hba.conf
+}
